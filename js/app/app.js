019637fc27b5a15a0dae8b08de02da81878d05c2
AM307 <mategka.andre@gmail.com>
2016-10-19 12:17:14 +0200

    Improve AngularJS dependency loading

--- js/app/app.js
+++ js/app/app.js
@@ -3,13 +3,18 @@
     var DEPENDENCIES = [
         'ui.router',
         'matheland.service',
-        'matheland.controller'
+        'matheland.controller',
+        'matheland.run'
     ];
 
     angular.module('matheland', DEPENDENCIES);
 
-    angular.module('matheland.controller', []);
-
-    angular.module('matheland.service', []);
+    for (var i = 0; i < DEPENDENCIES.length; i++) {
+        var dependency = DEPENDENCIES[i];
+        var packages = dependency.split('.');
+        if (packages[0] === 'matheland') {
+            angular.module(dependency, []);
+        }
+    }
 
 })();
