bfb1a38cf56a8184aa04f01848a04b1ef6bef18f
AM307 <mategka.andre@gmail.com>
2017-04-05 12:46:22 +0200

    Add NavController documentation

--- js/app/controller/nav.controller.js
+++ js/app/controller/nav.controller.js
@@ -6,10 +6,17 @@
         function NavController($state, Task) {
             var vm = this;
 
+            /**
+             * Returns whether or not the game has already been started.
+             * @returns {boolean} true if the navigation should be shown, false otherwise
+             */
             vm.showNav = function() {
                 return $state.current.name !== 'start';
             };
 
+            /**
+             * Aborts the current game and redirects the player to the start page.
+             */
             vm.goToStart = function() {
                 $state.go(Task.START);
             }
